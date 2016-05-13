c0a7fdc876d6b6bcd97894402b66728836b03017
Lukas <Lukasbarry@yahoo.com>
2016-05-13 07:46:05 -0400

    updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :condition
   belongs_to :user
+  has_many :line_items
 
   has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,9 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
-  geocoded_by :address
+  geocoded_by :zip_code
   after_validation :geocode
 
   has_many :products
+  has_many :carts
 end
